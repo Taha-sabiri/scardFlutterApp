a51f00899d47d9aec30f458c341b3104bef664df
Taha Sabiri <121357512+Taha-sabiri@users.noreply.github.com>
2023-07-23 00:43:33 +0330

    card done

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,6 +11,69 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlipCard(
+            fill: Fill
+                .fillBack, // Fill the back side of the card to make in the same size as the front.
+            direction: FlipDirection.HORIZONTAL, // default
+            side: CardSide.FRONT, // The side to initially display.
+            front: Container(
+                child: Stack(
+              children: [
+                Image.asset('assets/images/card.png'),
+                Positioned(
+                    top: 100,
+                    left: 90,
+                    child: Text(
+                      "2020 1543 3264 8645",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    )),
+                Positioned(
+                    top: 170,
+                    left: 90,
+                    child: Text(
+                      "طاها صبیری",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontFamily: "kalame",
+                          fontWeight: FontWeight.w700),
+                    )),
+                Positioned(
+                    top: 190,
+                    left: 90,
+                    child: Text(
+                      "TA241O",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontFamily: "kalame",
+                          fontWeight: FontWeight.w400),
+                    )),
+              ],
+            )),
+            back: Container(
+              child: Stack(
+                children: [
+                  Image.asset(
+                    'assets/images/cardback.png',
+                  ),
+                  Positioned(
+                    left: 70,
+                    top: 70,
+                    child: SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Image.asset('assets/images/qrcode.png')),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
